4e367de0bef284282d958409ffb5ce2a62ccde60
Gourab <linkgourab@gmail.com>
2026-02-06 15:48:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+// NEW VERSION TO FORCE UPDATE
+const CACHE_NAME = 'gourab-v6-new-icon'; 
+
+const ASSETS = [
+  '/',
+  '/index.html',
+  // Removed local icon path since we use direct URL now
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap'
+];
+
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys
+        .filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    fetch(evt.request).catch(() => {
+      return caches.match(evt.request);
+    })
+  );
+});
